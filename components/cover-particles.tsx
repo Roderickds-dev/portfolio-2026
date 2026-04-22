1a5ec158f90427ce14d327b26b3f847008d076c2
Roderick <roderickds.dev@gmail.com>
2026-04-22 14:54:05 -0300

    feat(): cover-particles

--- components/cover-particles.tsx
+++ components/cover-particles.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 const CoverParticles = () => {
-  return (
-    <div>CoverParticles</div>
-  )
-}
+  const [init, setInit] = useState(false);
 
-export default CoverParticles
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    init && (
+      <div className="w-0">
+        <Particles
+          id="tsparticles"
+          options={{
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      </div>
+    )
+  );
+};
+
+export default CoverParticles;
